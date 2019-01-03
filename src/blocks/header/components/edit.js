379bd1463c9d075dc2e8b02545ac3aa58ecdafd0
Krishna Kant <lushkant@gmail.com>
2019-01-04 01:14:51 +0530

    Convert edit object into a esnext function

--- src/blocks/header/components/edit.js
+++ src/blocks/header/components/edit.js
@@ -1,42 +1,22 @@
 /**
- * External dependencies
- */
-
-/**
  * WordPress dependencies
  */
 const { __ } = wp.i18n;
 const { Component, Fragment } = wp.element;
 
 /**
- * Block edit function
+ * Block edit function.
  */
-class Edit extends Component {
-	render() {
-		const { attributes, className, isSelected, setAttributes } = this.props;
+const edit = props => {
+	const { attributes, className, isSelected, setAttributes } = props;
 
-		return [
-			// eslint-disable-next-line react/jsx-key
-			<Fragment>
-				<div className={ className }>
-					<p>— Hello from the backend.</p>
-					<p>
-						CGB BLOCK: <code>thyself-blocks</code> is a new
-						Gutenberg block
-					</p>
-					<p>
-						It was created via{ ' ' }
-						<code>
-							<a href="https://github.com/ahmadawais/create-guten-block">
-								create-guten-block
-							</a>
-						</code>
-						.
-					</p>
-				</div>
-			</Fragment>,
-		];
-	}
-}
+	return (
+		<Fragment>
+			<div className={ className }>
+				<p>Something something</p>
+			</div>
+		</Fragment>
+	);
+};
 
-export default Edit;
+export default edit;
